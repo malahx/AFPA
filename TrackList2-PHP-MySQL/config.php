eee0a2323d6e8d77da09cfbc8d369ad6e3893e4c
Malah <malah@neuf.fr>
2016-11-05 20:46:31 +0100

    fusionnement de TrackList 1/2

--- TrackList2-PHP-MySQL/config.php
+++
@@ -1,6 +0,0 @@
-<?php 
-
-define('BDD_DSN',       'mysql:host=localhost;dbname=tracklist2;charset=utf8');
-define('BDD_USERNAME',  'root');
-define('BDD_PASSWORD',  'admin');
-define('UPLOAD','uploads/');
