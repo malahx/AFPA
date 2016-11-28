7a80f0cec67135ea709e230bbf5c3d587d987f15
Malah <malah@neuf.fr>
2016-11-28 09:47:36 +0100

    suppression de la fonctions refresh, correction de variable

--- TrackList-PHP-MySQL/controller/userRegister.php
+++ TrackList-PHP-MySQL/controller/userRegister.php
@@ -9,15 +9,15 @@ $password = filter_input(INPUT_POST, 'password', FILTER_SANITIZE_SPECIAL_CHARS);
 
 $existUser = userDAO::getByName($username);
 if ($existUser == -1) {
-    refresh('showAllPlaylists.php?fct=baduser');
+    header('showAllPlaylists.php?fct=baduser');
 }
 
 $user = new User(0, $username, $email, password_hash($password, PASSWORD_DEFAULT));
 
 $userId = userDAO::insert($user);
 
-if ($userid != -1) {
-    $_SESSION['userid'] = (int) $userid;
+if ($userId != -1) {
+    $_SESSION['userid'] = (int) $userId;
     $_SESSION['username'] = $user->getName();
 }
 header('Location: index.php');
