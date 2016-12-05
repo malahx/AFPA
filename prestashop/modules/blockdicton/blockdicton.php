6ba08a7793b301f53ef1e4ca9b18cc57ead53f44
Malah <malah@neuf.fr>
2016-12-05 16:00:32 +0100

    ajout commentaire, exemple d'installation à la journée actuelle, suppression de commentaire inutile

--- prestashop/modules/blockdicton/blockdicton.php
+++ prestashop/modules/blockdicton/blockdicton.php
@@ -8,6 +8,7 @@ class BlockDicton extends Module {
 
     public function __construct() {
 
+        // Variables du module
         $this->name = 'blockdicton';
         $this->tab = 'front_office_features';
         
@@ -23,6 +24,7 @@ class BlockDicton extends Module {
         
         parent::__construct();
         
+        // Affichage admin
         $this->displayName = $this->l('Dicton');
         $this->description = $this->l('Un jour, Un dicton !');
         $this->confirmUninstall = $this->l('Êtes vous certain de vouloir désinstaller ?');
@@ -31,10 +33,13 @@ class BlockDicton extends Module {
 
     public function install() {
 
+        // Configuration de la table
         Configuration::updateValue('BLOCKDICTON_TABLE', 'dicton');
 
+        // Variable de la table
         $table = Configuration::get('BLOCKDICTON_TABLE');
         
+        // Création de la table
         $created = Db::getInstance()->Execute('CREATE TABLE ' . _DB_PREFIX_ . $table . ' (
             `mois` int(11) NOT NULL,
             `jour` int(11) NOT NULL,
@@ -44,29 +49,36 @@ class BlockDicton extends Module {
             `conseil` text NOT NULL
             ) ENGINE=InnoDB DEFAULT CHARSET=latin1');
         
+        // Présaisie de 3 dictons en fonction d'aujourd'hui
         if ($created) {
             
+            $date = new DateTime();
+            
             Db::getInstance()->insert($table, array(
-                'mois' => 12,
-                'jour' => 5,
+                'mois' => $date->format('n'),
+                'jour' => $date->format('j'),
                 'saint' => "Samson",
                 'genre' => 1,
                 'dicton' => "Il est trop tard pour acheter du bois, quand l\'homme souffle sur ses doigts.",
                 'conseil' => "Il est temps de planter des arbres :)"
             ));
             
+            $date->add(new DateInterval('P1D'));
+            
             Db::getInstance()->insert($table, array(
-                'mois' => 12,
-                'jour' => 6,
+                'mois' => $date->format('n'),
+                'jour' => $date->format('j'),
                 'saint' => 'Malo',
                 'genre' => 1,
                 'dicton' => "Qui voit Ouessant voit son sang.",
                 'conseil' => "Il est temps de planter des fleurs :)"
             ));
             
+            $date->add(new DateInterval('P1D'));
+
             Db::getInstance()->insert($table, array(
-                'mois' => 12,
-                'jour' => 7,
+                'mois' => $date->format('n'),
+                'jour' => $date->format('j'),
                 'saint' => 'Brieuc',
                 'genre' => 1,
                 'dicton' => "Mieux vaut sagesse que richesse.",
@@ -75,6 +87,7 @@ class BlockDicton extends Module {
             
         }
         
+        // Retour afin de vérifier si l'installation s'est bien passée
         return parent::install() &&
                 $this->registerHook('leftColumn') &&
                 $this->registerHook('header') && $created;
@@ -82,30 +95,39 @@ class BlockDicton extends Module {
 
     public function uninstall() {
 
+        // Variable de la table
         $table = _DB_PREFIX_ . Configuration::get('BLOCKDICTON_TABLE');
         
+        // Essai de désinstallation des données du parent
         $parent = parent::uninstall();
         
+        // Suppression de la table
         if ($parent) {
             $deleted = Db::getInstance()->Execute('DROP TABLE ' . $table);
         }
         
+        // Retour afin de vérifier si la désinstalltion s'est bien passée
         return $parent && configuration::deleteByName('BLOCKDICTON_TABLE') && $deleted;
     }
 
     public function hookDisplayLeftColumn($params) {
         
+        // Variable de la table
         $table = _DB_PREFIX_ . Configuration::get('BLOCKDICTON_TABLE');
         
+        // Récupération du moi et du jour actuelle
         $mois = (int) date('n');
         $jour = (int) date('j');
         
+        // Requète pour récupérer le dicton, le saint et le conseil
         $sql = 'SELECT * FROM ' . $table . ' WHERE mois = ' . $mois . ' AND jour = ' . $jour;
         
         if ($row = Db::getInstance()->getRow($sql)) {
-
+            
+            // ajout du genre
             $saint = ($row['genre'] ? 'Saint' : 'Sainte') . ' ' . $row['saint'];
 
+            // Passage de variable à smarty
             $this->context->smarty->assign(array(
                 'saint' => $saint,
                 'dicton' => $row['dicton'],
@@ -113,24 +135,21 @@ class BlockDicton extends Module {
             ));
         }
 
+        // Affichage du template
         return $this->display(__FILE__, 'blockdicton.tpl');
     }
 
     public function hookDisplayRightColumn($params) {
 
+        // Effectuer comme sur la colonne de gauche
         return $this->hookDisplayLeftColumn($params);
     }
 
     public function hookDisplayHeader() {
 
+        // Ajout du css
         $this->context->controller->addCSS($this->_path . 'views/css/blockdicton.css', 'all');
         
     }
 
 }
-
-//Configuration::get('maVariable')
-//Configuration::updateValue('MYMODULE_NAME', 'my friend') 
-//Configuration::getMultiple(array('maPremiereVariable', 'maSecondeVariable', 'maTroisiemeVariable'))
-//Configuration::deleteByName('myVariable')
-// Conf de la langue : Configuration::get('PS_LANG_DEFAULT')
