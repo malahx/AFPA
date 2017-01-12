ccf3a88bb5f54d720450dbca478fcb7b983b574e
Malah <malah@neuf.fr>
2017-01-12 14:36:01 +0100

    ajout des valeurs aléatoires, ajout de la gestion des doubles, passage en string, ajout d'infos

--- Java/MasterCLIMind/src/masterclimind/MasterCLIMind.java
+++ Java/MasterCLIMind/src/masterclimind/MasterCLIMind.java
@@ -6,6 +6,7 @@
 package masterclimind;
 
 import java.util.Scanner;
+import java.util.concurrent.ThreadLocalRandom;
 
 /**
  *
@@ -21,13 +22,16 @@ public class MasterCLIMind {
     public static void main(String[] args) {
         sc = new Scanner(System.in);
 
+        mainloop:
         while (true) {
-            char[] soluce = {'1', '2', '3', '4'};
+            String soluce = rand(4, 1, 8);
             int i = 10;
             while (i > 0) {
                 int good = 0;
                 int wrong = 0;
                 System.out.println("------------------------------------------------");
+                System.out.println("------ Entrez 'exit' pour quitter");
+                System.out.println("Nombre de chiffre attendue : " + soluce.length());
                 System.out.println(Color.GREEN + "* les bonnes positions" + Color.RESET);
                 System.out.println(Color.RED + "* les mauvaises positions" + Color.RESET);
                 System.out.println("Tentez de trouver la solution (" + i + "):");
@@ -35,31 +39,64 @@ public class MasterCLIMind {
                 if (str.length() != 4) {
                     continue;
                 }
+                if (str.equals("exit")) {
+                    break mainloop;
+                }
+                String treated = "";
                 for (int j = 0; j < 4; j++) {
                     char car = str.charAt(j);
-                    if (soluce[j] == car) {
+                    if (count(treated, car) >= count(soluce, car)) {
+                        continue;
+                    }
+                    if (soluce.charAt(j) == car) {
                         good++;
+                        treated += soluce.charAt(j);
                         continue;
                     }
                     if (contains(soluce, car)) {
                         wrong++;
+                        treated += car;
                     }
                 }
                 String goodStr = new String(new char[good]).replace("\0", "*");
                 String wrongStr = new String(new char[wrong]).replace("\0", "*");
                 System.out.println(Color.GREEN + goodStr + Color.RED + wrongStr + Color.RESET);
+                if (good == 4) {
+                    System.out.println(Color.YELLOW + "BRAVO, vous avez gagné !!!");
+                    break;
+                }
                 i--;
             }
+            System.out.println(Color.YELLOW + "La solution était : " + soluce);
         }
     }
 
-    public static boolean contains(char[] cars, char car) {
+    private static boolean contains(String str, char car) {
+        char[] cars = str.toCharArray();
         for (char c : cars) {
             if (car == c) {
                 return true;
             }
         }
         return false;
+    }
+
+    private static int count(String str, char car) {
+        int j = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == car) {
+                j++;
+            }
+        }
+        return j;
+    }
+
+    private static String rand(int i, int min, int max) {
+        String str = new String();
+        for (int j = 0; j < i; j++) {
+            str += ThreadLocalRandom.current().nextInt(min, max + 1);
+        }
+        return str;
     }
 
 }
